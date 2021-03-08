586235abdbad7b4bcf180aa6e8c6c969295145de
Renata Santos <renatados.santosp@gmail.com>
2021-03-08 02:32:35 -0300

    Update N_válido.py

--- N_válido.py
+++ N_válido.py
@@ -2,4 +2,6 @@
 #Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido. 
 
 n = int(input('Dê uma nota para o atendimento (0-10): '))
-while n
+while n > 10:
+    n = int(input('Entrada inválida. Por favor, dê uma nota de 0 a 10 para o atendimento: '))
+print('Sua opinião é muito importante. Obrigada pela colaboração.')

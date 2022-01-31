dd3cabd193398f757ad100bbb4f5eb70f66da96d
Renata Santos <renatados.santosp@gmail.com>
2022-01-31 12:00:14 -0300

    update console reference types

---
+++ CursoCertificaçãoCSharp/certificacaoParte1/Aula1 - Tipos de Valor/1 - Tipos de Valor/TiposDeValor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacaoCSharpRoteiro
+{
+    class TiposDeValor : IAulaItem
+    {
+        public void Executar()
+        {
+            int idade = 25;
+
+            Console.WriteLine(idade);
+        }
+    }
+}
